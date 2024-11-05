bfe7cdf72c21c31df158088b202bb8ffd499899c
Diego Cattelan <diego@sunnyday.software>
2024-11-05 13:13:23 +0100

    Allow to specify astro project name. Great astro init experience

--- dev/scripts/run-astro-project.sh
+++ dev/scripts/run-astro-project.sh
@@ -6,10 +6,12 @@ set -e
 echo "script $0"
 
 source "$(dirname "$0")/env.sh"
+echo "Astro project: ${astro_project_name}"
+astro_full_path="${astro_path}/${astro_project_name}"
 
 check_container;
 
-cd ${astro_path}
+cd ${astro_full_path}
 
 npm install
 npm run dev
